7f0b890848e271941e09408ed679df7aa1daeb39
Danny JeanLouis <dlouis3386@gmail.com>
2022-04-20 12:08:47 -0400

    add appbar

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import WelcomeMessage from "./WelcomeMessage";
 import AppLayout from "./AppLayout";
+import AppBar from "./AppBar";
 
 function App() {
   return (
     <AppLayout>
+    <AppBar />
       <WelcomeMessage />
     </AppLayout>
   );
